f1b794bdfd72ad39f7123a155b7210427f811220
githubtteo <matteo.poljaric@gmail.com>
2019-01-15 14:40:24 +0100

    anfang schießen

--- INFO PROJEKT/Detektiv.java
+++ INFO PROJEKT/Detektiv.java
@@ -1,13 +1,13 @@
 import greenfoot.*;
 
-/**
+    /**
  * Write a description of class Detektiv here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Detektiv extends Actor
-{
+    {
     /**
      * Act - do whatever the Detektiv wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -21,25 +21,27 @@ public class Detektiv extends Actor
         
     
     }    
-    public void linksGehen()
+    public void jaGehen()
     {
-       if (Greenfoot.isKeyDown("a"))
+       if (Greenfoot.isKeyDown("left"))
        {
          setLocation(getX()-1, getY());
-         
+         setRotation (getRotation() -5);
        }  
-    }
-     public void rechtsGehen()
-    {
-     if (Greenfoot.isKeyDown("d"))
+       if (Greenfoot.isKeyDown("right"))
        {
          setLocation(getX()+1, getY());
-         
+         setRotation (getRotation() +5);
        }  
+       move(3);
+       if ("space".equals(Greenfoot.getKey()))
+        {
+         fire();
+        }    
     }
     public void obenGehen()
     {
-       if (Greenfoot.isKeyDown("w"))
+       if (Greenfoot.isKeyDown("up"))
        {
          setLocation(getX(), getY()-1);
         
@@ -47,11 +49,18 @@ public class Detektiv extends Actor
     }
     public void untenGehen()
     {
-       if (Greenfoot.isKeyDown("s"))
+       if (Greenfoot.isKeyDown("down"))
        {
          setLocation(getX(), getY()+1);
          
        }  
     }
+    private void fire()
+    {
+       Steel steel = new Steel();
+       getWorld().addObject(steel, getX(), getY());
+       steel.setRotation(getRotation());
+       steel.move(40.0);
    
     }
+}
